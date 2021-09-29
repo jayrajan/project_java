206dc8e308002bcd68303f9fa2e0874a5f5373aa
Jerin Rajan <jrajan@jprtech.co.uk>
2021-09-29 19:53:52 +0100

    first commit

---
+++ core/src/main/java/com/currencyconverter/App.java
@@ -0,0 +1,14 @@
+package com.currencyconverter;
+
+/**
+ * Currency converter 
+ * Code written by J Rajan on 29th Sep 2021
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is a currency converter project" );
+        System.out.println( " First Java Project" );
+    }
+}
